fb85e9b3e6be7ec6c4313ff99ca7c99eaea7b507
James Bowden <jamesbowd@gmail.com>
2021-03-27 15:40:56 +0000

    Added setInterval to keep the clock updating

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const hourHand = document.querySelector(".hour-hand");
 const minuteHand = document.querySelector(".min-hand");
 const secondHand = document.querySelector(".second-hand");
 
-let date = new Date();
+const showTime = () => {
+  let date = new Date();
 
-let hour = date.getHours();
-let minute = date.getMinutes();
-let second = date.getSeconds();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
 
-hourRotate = (hour / 12) * 360 + 90;
-hourHand.style.transform = `rotate(${hourRotate}deg)`;
+  hourRotate = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourRotate}deg)`;
 
-minuteRotate = (minute / 60) * 360 + 90;
-minuteHand.style.transform = `rotate(${minuteRotate}deg)`;
+  minuteRotate = (minute / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minuteRotate}deg)`;
 
-secondRotate = (second / 60) * 360 + 90;
-secondHand.style.transform = `rotate(${secondRotate}deg)`;
+  secondRotate = (second / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondRotate}deg)`;
+};
+
+showTime(0);
+
+setInterval(() => {
+  showTime();
+}, 1000);
