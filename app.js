6b4283411e3b820c3505492793145fcc560de70d
James Bowden <jamesbowd@gmail.com>
2021-03-27 15:35:06 +0000

    setup clock to display time on page refresh

---
+++ app.js
@@ -0,0 +1,18 @@
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".min-hand");
+const secondHand = document.querySelector(".second-hand");
+
+let date = new Date();
+
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+
+hourRotate = (hour / 12) * 360 + 90;
+hourHand.style.transform = `rotate(${hourRotate}deg)`;
+
+minuteRotate = (minute / 60) * 360 + 90;
+minuteHand.style.transform = `rotate(${minuteRotate}deg)`;
+
+secondRotate = (second / 60) * 360 + 90;
+secondHand.style.transform = `rotate(${secondRotate}deg)`;
